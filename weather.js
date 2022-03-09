bf26855410c00300e12df079957efd09c9a8fbc8
Alexander Horta <alexhortariv@gmail.com>
2022-03-10 00:53:33 +0700

    Работа с командной строкой

---
+++ weather.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import {getArgs} from './helpers/args.js'
+import {printHelp} from "./services/log.services.js";
+
+const initCLI = () => {
+
+    const args = getArgs(process.argv);
+
+    if(args.h) {
+        printHelp();
+    }
+    if(args.s) {
+        // Сохранить город
+    }
+    if(args.t) {
+        // Сохранить токен
+    }
+    // Вывести погоду
+
+};
+
+initCLI();
